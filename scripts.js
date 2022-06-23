0f33d524a4b7feea969729f5b76937403a5e9703
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-06-23 13:34:03 -0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+console.log('Hello, world')
+
+let today = new Date();
+var day = today.getDay();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+var date = today.getMonth()+1+'-'+(today.getDate())+'-'+today.getFullYear();
+var time = today.getHours() + ":" + today.getMinutes();
+var dateTime = date+' and it is '+time + " o'clock";
+ 
+document.getElementById("displayDateTime").innerHTML = ' Today is ' + daylist[day] + dateTime;
