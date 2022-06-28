9fa7145e68ada3f5333f0ebe0f3016d68b3d5c1b
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-06-28 12:27:35 -0700

    add js

--- scripts.js
+++ scripts.js
@@ -8,3 +8,12 @@ var time = today.getHours() + ":" + today.getMinutes();
 var dateTime = date+' and it is '+time + " o'clock";
  
 document.getElementById("displayDateTime").innerHTML = ' Today is ' + daylist[day] + dateTime;
+
+function displayDescription() {
+    var x = document.getElementById("description");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } 
