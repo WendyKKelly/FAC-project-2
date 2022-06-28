2e06a32f2d9019bff8c2263eeb896269b439fb25
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-06-28 15:44:26 -0700

    made 6 separate functions

--- scripts.js
+++ scripts.js
@@ -9,7 +9,56 @@ var dateTime = date+' and it is '+time + " o'clock";
  
 document.getElementById("displayDateTime").innerHTML = ' Today is ' + daylist[day] + dateTime;
 
- function displayDescription(){
+ function disDesc(){
+    const description = document.querySelector(".description");
+    if (description.style.display === "none") {
+     description.style.display = "block";
+    } else {
+        description.style.display = "none";
+        
+    }
+  } 
+  function disDesc2(){
+    const description = document.querySelector(".description");
+    if (description.style.display === "none") {
+     description.style.display = "block";
+    } else {
+        description.style.display = "none";
+        
+    }
+  } 
+
+  function disDesc3(){
+    const description = document.querySelector(".description");
+    if (description.style.display === "none") {
+     description.style.display = "block";
+    } else {
+        description.style.display = "none";
+        
+    }
+  } 
+
+  function disDesc4(){
+    const description = document.querySelector(".description");
+    if (description.style.display === "none") {
+     description.style.display = "block";
+    } else {
+        description.style.display = "none";
+        
+    }
+  } 
+
+  function disDesc5(){
+    const description = document.querySelector(".description");
+    if (description.style.display === "none") {
+     description.style.display = "block";
+    } else {
+        description.style.display = "none";
+        
+    }
+  } 
+
+  function disDesc6(){
     const description = document.querySelector(".description");
     if (description.style.display === "none") {
      description.style.display = "block";
